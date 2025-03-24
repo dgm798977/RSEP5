d11f60d111bfd0293a8e4a5bddc516ec4dddb910
dgm798977 <798977@unizar.es>
2025-03-24 20:18:35 +0100

    Update config.h

--- 10_MQTT/subscribe/config.h
+++ 10_MQTT/subscribe/config.h
@@ -3,7 +3,7 @@
 // visit io.adafruit.com if you need to create an account,
 // or if you need your Adafruit IO key.
 #define IO_USERNAME "test"
-
+#define IO_KEY "test"
 
 /******************************* WIFI **************************************/
 
